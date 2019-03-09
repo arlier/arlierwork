6cc658fa3dcef2ebca67b6a951fb110861cba959
arlier <1278118931@qq.com>
2019-03-09 14:05:26 +0800

    2019-03-09-14-05-1552111526-arlier

--- commit.sh
+++ commit.sh
@@ -1,5 +1,8 @@
+#!/bin/bash
+#PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+cd /media/tao/_dde_data/arlierwork
 git add .
-mydate= date "+%Y-%m-%d-%H:%M:%S.%N"
-echo $mydate
+#mydate= date "+%Y-%m-%d-%H:%M:%S.%N"
+#echo $mydate
 git commit -m `date +%Y-%m-%d-%H-%M-%s`-arlier
 git push -u origin master
