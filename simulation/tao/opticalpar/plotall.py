f297cc12ca5852396dc4e2f99fbd55fece2d94f5
arlier <1278118931@qq.com>
2019-03-19 09:55:01 +0800

    2019-03-19-09-55-1552960501-arlier

--- simulation/tao/opticalpar/plotall.py
+++ simulation/tao/opticalpar/plotall.py
@@ -6,18 +6,25 @@ import pylab as pl
 f = open('/media/tao/_dde_data/arlierwork/simulation/tao/opticalpar/ej200out.txt')
 f1 = open('/media/tao/_dde_data/arlierwork/simulation/tao/opticalpar/pmtout.txt')
 s = f.readline()
+s1 = f1.readline()
 a1=[]
 a2=[]
+b1=[]
+b2=[]
 count=0
 while (count<301):
      arr=s.split(' ')
+     arr1=s1.split(' ')
 #     print arr
 #     print arr[1]
 #     a1=arr[0]
      a1.append(float(arr[0]))
+     b1.append(float(arr1[0]))
      a2.append(float(arr[1].replace('\r\n',''))) #readline 读取文件的时候，默认加上“\n"
+     b2.append(float(arr1[1].replace('\r\n',''))) #readline 读取文件的时候，默认加上“\n"
 #     a2=arr[1].replace('\n','') #readline 读取文件的时候，默认加上“\n"
      s=f.readline()
+     s1=f1.readline()
      count+=1
 pl.figure(figsize=(8,8))
 #axes = pl.plot(111)
@@ -31,6 +38,7 @@ pl.grid(True)
 #pl.axis([350,550,0,1])
 pl.xlabel('wave length [nm]',color='k',fontsize=15,rotation=0) 
 pl.ylabel('light output [A.U]',color='k',fontsize=15,rotation=90) 
+pl.plot(b1,b2,".",label='pmt',c=(0,0.0,0.0),alpha=0.5,linestyle="-")
 pl.legend(loc="upper right")
 pl.savefig("allplot.eps")
 pl.show()
